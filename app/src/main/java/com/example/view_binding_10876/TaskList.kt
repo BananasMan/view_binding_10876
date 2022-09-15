3df71a9edc5d741dcd25b0a4a9ca7f34b34201b9
BananasMan <uchihaericko@gmail.com>
2022-09-15 08:12:05 +0700

    view binding

---
+++ app/src/main/java/com/example/view_binding_10876/TaskList.kt
@@ -0,0 +1,17 @@
+package com.example.view_binding_10876
+
+object TaskList {
+    val tasklist = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur",
+            "Kebiasaan  sehari-hari untuk membuat hari makin indah"),
+        Task("Chapter 2", "Ngopi",
+            "Grinding  exp buat level up alpha"),
+        Task("Chapter 3", "Bangun Tidur",
+            "Habis capek Nguli, enaknya sarapan"),
+        Task("Chapter 4", "Frog Plushie",
+            "Grinding boneka favorit alpha, lagi"),
+        Task("Chapter 5", "Upload ke Youtube",
+            "Alpha solo boss tour, Alpha vs Lamia"),
+
+        )
+}
